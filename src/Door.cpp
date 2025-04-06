10cc016001aedc753b1ce63a2a384684e86d764d
Nawal Khawar <nawal.khawar@gmail.com>
2025-04-06 15:50:02 +0500

    Update Door.cpp

--- src/Door.cpp
+++ src/Door.cpp
@@ -1,1 +1,31 @@
 
+#include "Door.h"
+
+Door::Door()
+{
+	isOpen = false;
+}
+
+//return door state
+bool Door::checkIfOpen()
+{
+	if (isOpen) //door is open
+	{
+		return true;
+	}
+	else //door is closed
+	{
+		return false;
+	}
+}
+
+//open/close the door
+void Door::toggle()
+{
+	isOpen = !isOpen;
+}
+
+Door::~Door()
+{
+
+}
